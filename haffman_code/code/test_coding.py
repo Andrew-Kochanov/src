a8e80ee4c6e761981cf275e6d4096017249922ae
Andrew-Kochanov <andkochanov2019@gmail.com>
2025-12-20 20:05:21 +0300

    Занёс функции создания файлов для проверки в тест

--- haffman_code/code/test_coding.py
+++ haffman_code/code/test_coding.py
@@ -1,16 +1,14 @@
 from encoding import encode_file
 from decoding import decode_file
 
-encode_file("origin_file.txt", "encoding_file.txt")
-decode_file("encoding_file.txt", "decoding_file.txt")
-
 def make_file(str):
     with open(str, 'w', encoding = 'utf-8') as file:
         file.write('Hello, World!')
 
 def test_unit():
     make_file('origin_file.txt')
+    encode_file("origin_file.txt", "encoding_file.txt")
+    decode_file("encoding_file.txt", "decoding_file.txt")
     origin_file = open('origin_file.txt').read()
     decoding_file = open('decoding_file.txt').read()
     assert origin_file == decoding_file
-
